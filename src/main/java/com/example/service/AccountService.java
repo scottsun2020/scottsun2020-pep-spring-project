5edb9e2d66be746de86895349d5005283d66567d
Scott Sun <sunyadong126@gmail.com>
2024-01-10 07:20:14 +0000

    First Commit

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,8 +1,14 @@
 package com.example.service;
 
-import com.example.entity.Account;
+import org.springframework.stereotype.Service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+    
 
     public Account addAccount(Account newAccount) {
         return null;
