359e608d89ee771bc9029da498b65f008aa347b2
Scott Sun <sunyadong126@gmail.com>
2024-01-11 02:28:46 +0000

    Message Service is done

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,13 @@
 package com.example.repository;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.example.entity.Message;
+
 public interface MessageRepository {
+
+    ArrayList<Message> getAllMessages();
+
+    Optional<Message> getMessageById(int id);
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,20 +1,36 @@
 package com.example.service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.example.entity.Message;
+import com.example.repository.MessageRepository;
 
-@Component
+@Service
 public class MessageService {
+    //dependency
+    MessageRepository messageRepository;
 
-    public ArrayList<Message> getAllMessages() {
-        return null;
+    @Autowired
+    public MessageService (MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
     }
 
-    public Message getMessageById() {
-        return null;
+    public ArrayList<Message> getAllMessages() {
+        return this.messageRepository.getAllMessages();
+    }
+
+    public Message getMessageById(int id) {
+        Optional<Message> optionalMessage = this.messageRepository.getMessageById(id);
+        if(optionalMessage.isPresent()){
+            return optionalMessage.get();
+        }else{
+            return null;
+        }
     }
 
     public Message addMessage(Message message) {
